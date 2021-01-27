658b0cf8be3f7de535fcf2fbcc381429525aedb9
konatanaya <wukonatan@gmail.com>
2021-01-27 16:22:49 +0800

    modify details

--- files/publications.js
+++ files/publications.js
@@ -1,45 +1,17 @@
-var journals = [
-    {
-        "title":"Greencommute: An Influence-aware Persuasive Recommendation Approach for Public-friendly Commute Options",
-        "author": ["Shiqing Wu", "Quan Bai, and Sotsay Sengvong"],
-        "booktitle":"Journal of Systems Science and Systems Engineering",
-        "year":"2018",
-        "note":"",
-        "pdf":"",
-        "bibtex":""
-    },
-
-]
-
-var conferences = [
-    {
-        "title":"Learning Policies for Effective Incentive Allocation in Unknown Social Networks",
-        "author":["Shiqing Wu", "Quan Bai, and Weihua Li"],
-        "booktitle":"Proceedings of the 20th International Conference on Autonomous Agents and MultiAgent Systems (AAMAS)",
-        "year":"2021",
-        "note":"",
-        "pdf":"",
-        "bibtex":""
-    },
-    {
-        "title":"Incentivizing Long-term Engagement Under Limited Budget",
-        "author":["Shiqing Wu", "and Quan Bai"],
-        "booktitle":"PRICAI 2019: Trends in Artificial Intelligence (PRICAI)",
-        "year":"2019",
-        "note":"",
-        "pdf":"",
-        "bibtex":""
-    },
-    {
-        "title":"Adaptive Incentive Allocation for Influence-aware Proactive Recommendation",
-        "author":["Shiqing Wu", "Quan Bai, and Byeong Ho Kang"],
-        "booktitle":"PRICAI 2019: Trends in Artificial Intelligence (PRICAI)",
-        "year":"2019",
-        "note":"",
-        "pdf":"",
-        "bibtex":""
-    },
-]
+function generatePublicationList(list, type){
+    var length = list.length;
+    var str = "<dl class='row text-justify-left'>";
+    for(var i=0 ;i<length;i++){
+        var index = length-i;
+        var item = list[i];
+        var authors = getAuthors(item.author);
+        str += "<dd class='col-sm-1'>["+type+index+"]</dd><dd class='col-sm-11'>"+authors+". "+item.title+
+        ". <i>"+item.booktitle+"</i>, "+item.year+". <a href="+item.pdf+" target='_blank' class='btn btn-outline-light btn-xs disabled'>PDF</a> <a href="+
+        item.bibtex+" target='_blank' class='btn btn-outline-light btn-xs disabled'>Bibtex</a></dd>";
+    }
+    str += "</dl>";
+    return str;
+}
 
 function getAuthors(author_list){
     var a_length = author_list.length;
@@ -66,27 +38,29 @@ function getAuthors(author_list){
     return str;
 }
 
-function generatePublicationList(list, type){
-    var length = list.length;
-    var str = "<dl class='row text-justify-left'>";
-    for(var i=0 ;i<length;i++){
-        var index = length-i;
-        var item = list[i];
-        var authors = getAuthors(item.author);
-        str += "<dd class='col-sm-1'>["+type+index+"]</dd><dd class='col-sm-11'>"+authors+". "+item.title+
-        ". <i>"+item.booktitle+"</i>, "+item.year+". <a href="+item.pdf+" target='_blank' class='btn btn-outline-light btn-xs disabled'>PDF</a> <a href="+
-        item.bibtex+" target='_blank' class='btn btn-outline-light btn-xs disabled'>Bibtex</a></dd>";
-    }
-    str += "</dl>";
-    return str;
+function loadPublications(){
+    var journals = [];
+    var conferences = [];
+    $.getJSON("files/publications.json", function (data) {
+        $.each(data, function (index, item) {
+            console.log(item.type);
+            switch(item.type){
+                case("journal"):
+                    journals.push(item);
+                    break;
+                case("conference"):
+                    conferences.push(item);
+                    break;
+                default:
+                    break;
+            }
+        });
+        $("#journalDIV").append(generatePublicationList(journals,'J'))
+        $('#conferenceDIV').append(generatePublicationList(conferences, 'C'));
+    });
 }
 
 $(document).ready(function(){
-    var str = "<h3 class=\"section-title\">Publications</h3><h5 class='subsection-title'>Referred Journal Papers</h5>"+
-    generatePublicationList(journals,'J')+
-    "<h5 class='subsection-title'>Referred Conference Papers</h5>"+
-    generatePublicationList(conferences, 'C');
-
-    $('#publications').append(str);
+    loadPublications();
 })
 
