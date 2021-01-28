b72b1896108f5c7edb75a812ed15a7a6e2ad6837
konatanaya <wukonatan@gmail.com>
2021-01-28 23:53:54 +0800

    modify details

--- js/effect.js
+++ js/effect.js
@@ -42,3 +42,11 @@ $(document).ready(function(){
         }
     })
 });
+
+window.onload = function(){
+    $("a.btn-xs").each(function(index,obj){
+        if($(this).attr("href") == ""){
+            $(this).addClass('disabled');
+        }
+    });
+}
