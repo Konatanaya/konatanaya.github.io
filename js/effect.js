a74734f1fc34f6c89af431548ea4380691690447
konatanaya <wukonatan@gmail.com>
2021-01-31 18:14:10 +0800

    update framework

--- js/effect.js
+++ js/effect.js
@@ -49,4 +49,12 @@ window.onload = function(){
             $(this).removeClass('disabled');
         }
     });
+
+    var test = /-min\./;
+    $("img").each(function(index,obj){
+        if(test.test($(this).attr("src"))){
+            var reSrc = $(this).attr("src").replace(test,".");
+            $(this).attr("src",reSrc)
+        }
+    });
 }
