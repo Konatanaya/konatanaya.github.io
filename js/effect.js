5e8b74956599e2127a2e8fefb6a5ddb06dd571bf
konatanaya <wukonatan@gmail.com>
2021-02-06 12:16:14 +0800

    add preloader

--- js/effect.js
+++ js/effect.js
@@ -1,38 +1,212 @@
+var load_content_functions = [
+    function(){//news
+        function generateConferenceNews(item){
+            var str = "<li><b>["+item.date+"]</b><br>" + item.content + item.conference+".</li><br>";
+            return str;
+        }
+        function generateJournalNews(item){
+            var str = "<li><b>["+item.date+"]</b><br>" + item.content + item.journal+".</li><br>";
+            return str;
+        }
+        function generateWorkshopNews(item){
+            var str = "<li><b>["+item.date+"]</b><br>" + item.content + item.workshop+".</li><br>";
+            return str;
+        }
+        function generateNormalNews(item){
+            var str = "<li><b>["+item.date+"]</b><br>" + item.content +"</li><br>";
+            return str;
+        }
+        $.getJSON("data/news.json", function (data) {
+            $.each(data, function (index, item) {
+                switch(item.type){
+                    case("journal"):
+                        item_code = generateJournalNews(item);
+                        break;
+                    case("conference"):
+                        item_code = generateConferenceNews(item);
+                        break;
+                    case("workshop"):
+                        item_code = generateWorkshopNews(item);
+                        break;
+                    case("normal"):
+                        item_code = generateNormalNews(item);
+                    default:
+                        break;
+                }
+                $("#news-list").append(item_code)
+            });
+        });
+    },
+    function(){//publications
+        function generatePublicationList(list, type){
+        var length = list.length;
+        var str = "";
+        for(var i=0 ;i<length;i++){
+            var index = length-i;
+            var item = list[i];
+            var authors = getAuthors(item.author);
+            str += "<dd class='col-sm-1 pub-index'>["+type+index+"]</dd><dd class='col-sm-11' >"+authors+". "+item.title+
+            ". <em>"+item.booktitle+"</em>, "+item.year+". "+checkLink(item.pdf)+"PDF</a> "+
+             checkLink(item.bibtex)+"Bibtex</a></dd>";
+        }
+        return str;
+    }
+        function checkLink(link){
+            if(link == ""){
+                return "<a href='"+link+"' target='_blank' class='btn btn-outline-light btn-xs disabled'>";
+            }
+            else{
+                return "<a href='"+link+"' target='_blank' class='btn btn-outline-light btn-xs'>";
+            }
+        }
+        function getAuthors(author_list){
+            var a_length = author_list.length;
+            var str = "";
+            for(var i=0; i<a_length;i++){
+                a = author_list[i];
+                if (i==a_length-1){
+                    if (a=="Shiqing Wu"){
+                        str += "<b>"+a+"</b>";
+                    }
+                    else{
+                        str += a;
+                    }
+                }
+                else if (i<a_length-1){
+                    if (a=="Shiqing Wu"){
+                        str += "<b>"+a+"</b>, ";
+                    }
+                    else{
+                        str += a+", ";
+                    }
+                }
+            }
+            return str;
+        }
+        var journals = [];
+        var conferences = [];
+        $.getJSON("data/publications.json", function (data) {
+            $.each(data, function (index, item) {
+                switch(item.type){
+                    case("journal"):
+                        journals.push(item);
+                        break;
+                    case("conference"):
+                        conferences.push(item);
+                        break;
+                    default:
+                        break;
+                }
+            });
+            $("#journal-list").append(generatePublicationList(journals,'J'))
+            $('#conference-list').append(generatePublicationList(conferences, 'C'));
+        });
+    },
+    function(){//teaching
+        function generateTeachingItem(item){
+        var str = "<li>"+item.course+", "+item.time+".</li>";
+        return str;
+    }
+        $.getJSON("data/teaching.json", function (data) {
+        $.each(data, function (index, item) {
+            item_code = generateTeachingItem(item);
+            switch(item.institution){
+                case("UTAS"):
+                    $("#teaching-UTAS").append(item_code);
+                    break;
+                case("AUT"):
+                    $("#teaching-AUT").append(item_code);
+                    break;
+                default:
+                    break;
+            }
+        });
+    });
+    },
+    function(){//services
+        function generateServiceItem(item){
+        var str = "<li>"+item.role+", "+item.name+", "+item.year+".</li>";
+        return str;
+    }
+        $.getJSON("data/services.json", function (data) {
+        $.each(data, function (index, item) {
+            item_code = generateServiceItem(item);
+            switch(item.type){
+                case("journal"):
+                    $("#services-journal").append(item_code);
+                    break;
+                case("conference"):
+                    $("#services-conference").append(item_code);
+                    break;
+                default:
+                    break;
+            }
+        });
+    });
+    },
+    function(){//awards
+        function generateAwardsItem(item){
+            var str = "<li>"+item.item+", "+item.organization+", "+item.year+".</li>";
+            return str;
+        }
+        $.getJSON("data/awards.json", function (data) {
+            $.each(data, function (index, item) {
+                item_code = generateAwardsItem(item);
+                $("#awards-list").append(item_code);
+            });
+        });
+    }
+]
+
 // Effect of pre-loading bar
-;(function(){
-    function id(v){ return document.getElementById(v); }
-    function loadbar() {
+$(function(){
+    function id(v){
+        return document.getElementById(v);
+    }
+    function loadbar(){
         var ovrl = id("overlay"),
             prog = id("progress"),
             stat = id("progstat"),
-            content = document.all,
+            content = document.images;
             c = 0,
-            tot = content.length;
-        if(tot == 0) return doneLoading();
+            tot = content.length + load_content_functions.length;
+        if(tot == 0)
+            return doneLoading();
 
         function contentLoaded(){
-          c += 1;
-          var perc = ((100/tot*c) << 0) +"%";
-          prog.style.width = perc;
-          stat.innerHTML = "Loading "+ perc;
-          if(c===tot) return doneLoading();
+            c += 1;
+            var perc = ((100/tot*c) << 0) +"%";
+            prog.style.width = perc;
+            stat.innerHTML = "Loading "+ perc;
+            if(c===tot) return doneLoading();
         }
         function doneLoading(){
-          ovrl.style.opacity = 0;
-          setTimeout(function(){
-            ovrl.style.display = "none";
-          }, 1200);
+            ovrl.style.opacity = 0;
+            setTimeout(function(){
+                ovrl.style.display = "none";
+            }, 1200);
         }
-        for(var i=0; i<tot; i++) {
+
+        for(var i=0; i<load_content_functions.length; i++){
+            load_content_functions[i]();
             contentLoaded();
-//          var tImg = new Image();
-//          tImg.onload = contentLoaded;
-//          tImg.onerror = contentLoaded;
-//          tImg.src = content[i];
         }
-  }
-  document.addEventListener('DOMContentLoaded', loadbar, false);
+        for(var i=0; i<content.length; i++) {
+            var tImg = new Image();
+            tImg.onload = contentLoaded;
+            tImg.onerror = contentLoaded;
+            tImg.src = content[i];
+        }
+    }
+    document.addEventListener('DOMContentLoaded', loadbar, false);
 }());
+
+
+
+
+
+
+
 
 // Effect triggered when nav button is clicked
 $('nav ul li a').bind("click", function(e) {
