cc985c44dfccd78d8f4cf9ef3960dfb5e42919e8
konatanaya <wukonatan@gmail.com>
2021-02-06 10:22:30 +0800

    add preloader

--- js/effect.js
+++ js/effect.js
@@ -1,5 +1,4 @@
-
-
+// Effect triggered when nav button is clicked
 $('nav ul li a').bind("click", function(e) {
     var headerHeight = $("#navbar").height();
     var target = $(this).attr("href"); //Get the target
@@ -17,6 +16,7 @@ $(".nav .nav-link").on("click", function(){
     $(this).addClass("active");
 });
 
+// Button for expanding news panel when the screen size is smaller than lg
 $("#news-button").on("click", function(){
     if($("#news-span").html()==="Show News"){
         $("#news-span").text("Hide News");
@@ -26,10 +26,13 @@ $("#news-button").on("click", function(){
     }
 });
 
+// Function for resizing the width of the news panel with changing screen width.
 $(window).resize(function () {
     $('#news').width($('#news-col').width()-20);
 }).resize();
 
+//Function to reload high resolution images to replace the compressed one when all resources loaded completely,
+//aiming to reduce loading time at the client end.
 window.onload = function(){
     var test = /-min\./;
     $("img").each(function(index,obj){
@@ -39,3 +42,38 @@ window.onload = function(){
         }
     });
 }
+
+// Effect of pre-loading bar
+(function(){
+  function id(v){ return document.getElementById(v); }
+  function loadbar() {
+    var ovrl = id("overlay"),
+        prog = id("progress"),
+        stat = id("progstat"),
+        img = document.images,
+        c = 0,
+        tot = img.length;
+    if(tot == 0) return doneLoading();
+
+    function imgLoaded(){
+      c += 1;
+      var perc = ((100/tot*c) << 0) +"%";
+      prog.style.width = perc;
+      stat.innerHTML = "Loading "+ perc;
+      if(c===tot) return doneLoading();
+    }
+    function doneLoading(){
+      ovrl.style.opacity = 0;
+      setTimeout(function(){
+        ovrl.style.display = "none";
+      }, 1200);
+    }
+    for(var i=0; i<tot; i++) {
+      var tImg     = new Image();
+      tImg.onload  = imgLoaded;
+      tImg.onerror = imgLoaded;
+      tImg.src     = img[i].src;
+    }
+  }
+  document.addEventListener('DOMContentLoaded', loadbar, false);
+}());
